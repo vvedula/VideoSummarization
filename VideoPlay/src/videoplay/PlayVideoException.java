f6e2190e8e7e1a96a375d49ef6a7901f665d9640
vvedula <vishnu_vedula@yahoo.co.in>
2013-02-22 01:15:12 -0800

    init

---
+++ VideoPlay/src/videoplay/PlayVideoException.java
@@ -0,0 +1,23 @@
+package videoplay;
+
+
+
+public class PlayVideoException extends Exception{
+    
+    public PlayVideoException(){
+        System.out.println("Cant render video stream..");
+    }
+    
+    public PlayVideoException(String message) {
+	super(message);
+    }
+ 
+    public PlayVideoException(Throwable cause) {
+	super(cause);
+    }
+ 
+    public PlayVideoException(String message, Throwable cause) {
+	super(message, cause);
+    }
+ 
+}
